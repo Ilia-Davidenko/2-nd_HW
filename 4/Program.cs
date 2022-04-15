99551eb8cf215650c76c20dcf4a8b2125c4e5995
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-15 19:35:40 +0300

    4 task

---
+++ 4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите номер дня недели (1-7)");
+int day = int.Parse(ReadLine());
+
+if (day == 6) WriteLine("Выходной");
+if (day == 7) WriteLine("Выходной");
+if (day == 1) WriteLine("Будний день");
+if (day == 2) WriteLine("Будний день");
+if (day == 3) WriteLine("Будний день");
+if (day == 4) WriteLine("Будний день");
+if (day == 5) WriteLine("Будний день");
