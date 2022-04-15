cfff0129fce76b9587ea4c3214e23264af28a7a7
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-15 18:36:02 +0300

    3 task

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите любое число");
+int a = int.Parse(ReadLine());
+string str = a.ToString();
+
+if (a >= 100)
+{
+    WriteLine(str[2]);
+}
+else WriteLine("В числе нет третьей цифры");
