7efd46c2f52919435809ba4dd474dbbb09ca6945
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-15 17:39:03 +0300

    1 and 2 task

---
+++ 1/Program.cs
@@ -0,0 +1,5 @@
+﻿int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int sum1 = a / 10;
+int sum = sum1 % 10;
+Console.WriteLine(sum);
---
+++ 2/Program.cs
@@ -0,0 +1,5 @@
+﻿int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int sum = a / 100 * 10 + a % 10;
+Console.WriteLine(sum);
+
